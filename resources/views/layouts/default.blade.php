5c6c1516b146158a466ab601ebea95e74bd0ffd6
zmkm374 <n7186703@126.com>
2017-07-18 15:53:45 +0000

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>@yield('title','Sample')</title>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
